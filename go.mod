a64eef42b691aa602422e118a4a56a143c7b37d1
twice-forever <bnn0202@outlook.com>
2022-03-13 20:37:24 +0800

    feat: 用户表结构

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module tf-sso
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.6.3 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/labstack/echo/v4 v4.6.3 // indirect
+	xorm.io/xorm v1.2.5 // indirect
+)
--- model/user.go
+++ model/user.go
@@ -1,1 +1,32 @@
 package model
+
+import "time"
+
+type User struct {
+	Owner     string    `xorm:"varchar(100) notnull pk" json:"owner"`
+	Name      string    `xorm:"varchar(100) notnull pk" json:"name"`
+	CreatedAt int64     `xorm:"created" json:"created"`
+	UpdatedAt int64     `xorm:"updated" json:"updated"`
+	DeletedAt time.Time `xorm:"deleted"`
+
+	ID            string `xorm:"varchar(100) index" json:"id"`     // 用户id
+	Type          string `xorm:"varchar(100)" json:"type"`         // 用户类型
+	Password      string `xorm:"varchar(100)" json:"password"`     // 密码
+	PasswordSalt  string `xorm:"varchar(100)" json:"passwordSalt"` // 密码盐
+	DisplayName   string `xorm:"varchar(100)" json:"displayName"`  // 用户显示名称
+	Avatar        string `xorm:"varchar(500)" json:"avatar"`       // 头像
+	Email         string `xorm:"varchar(100) index" json:"email"`  // 电子邮件
+	Phone         string `xorm:"varchar(100) index" json:"phone"`  // 电话
+	Location      string `xorm:"varchar(100)" json:"location"`     // 地址
+	Bio           string `xorm:"varchar(100)" json:"bio"`          // 描述
+	Region        string `xorm:"varchar(100)" json:"region"`
+	Gender        string `xorm:"varchar(100)" json:"gender"`
+	IsOnline      bool   `json:"isOnline"`
+	IsAdmin       bool   `json:"isAdmin"`
+	IsGlobalAdmin bool   `json:"isGlobalAdmin"`
+	IsForbidden   bool   `json:"isForbidden"`
+	IsDeleted     bool   `json:"isDeleted"`
+
+	LastSigninTime string `xorm:"varchar(100)" json:"lastSigninTime"`
+	LastSigninIp   string `xorm:"varchar(100)" json:"lastSigninIp"`
+}
