02d5a850066ddc1eceb434f2c147c2e1973934ca
twice-forever <bnn0202@outlook.com>
2022-04-03 21:26:07 +0800

    feat: 增加菜单栏组件

--- web/src/view/userManage/index.vue
+++ web/src/view/userManage/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px"><Menu /></el-aside>
       <el-container>
         <el-header>Header</el-header>
-        <el-main>Main</el-main>
+        <el-main>
+          
+        </el-main>
         <el-footer>Footer</el-footer>
       </el-container>
     </el-container>
@@ -12,12 +14,12 @@
 </template>
 
 <script>
-// import HelloWorld from "@/components/HelloWorld.vue";
+import Menu from "@/components/menu/index.vue";
 
 export default {
   name: "UserManage",
   components: {
-    // HelloWorld,
+    Menu,
   },
 };
 </script>
